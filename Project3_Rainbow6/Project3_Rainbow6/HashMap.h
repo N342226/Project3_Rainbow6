cbcca3b414e0a43383c71ccd76e0c610d5fdbf71
Charlie Ulrich <Charlie@cydian.com>
2020-12-01 15:43:51 -0500

    Finished Hashmap

--- Project3_Rainbow6/Project3_Rainbow6/HashMap.h
+++ Project3_Rainbow6/Project3_Rainbow6/HashMap.h
@@ -21,7 +21,7 @@ public:
 	Hashmap();
 
 	void insertPlayer(player, vector<vector<player>>&);
-	void removePlayer(player);
+	bool removePlayer(string name);
 	int hashPlayer(player, int);
 	player retrievePlayerInfo(string);
 	vector<vector<player>> resize();
@@ -42,15 +42,22 @@ void Hashmap::insertPlayer(player insert, vector<vector<player>>& hash) {
 	}
 }
 
-void Hashmap::removePlayer(player) {
-
+bool Hashmap::removePlayer(string name) {
+	for (int i = 0; i < this->hashMapBuckets.size(); i++){
+		for (int z = 0; z < this->hashMapBuckets.at(i).size(); z++) {
+			if (this->hashMapBuckets.at(i).at(z).getName() == name)
+			{
+				this->hashMapBuckets.at(i).erase(this->hashMapBuckets.at(i).begin()+z);
+			}
+		}	
+	}
+	return false;
 }
 
 int Hashmap::hashPlayer(player unHashed, int buckets) {
 	long sum = 0;
 	long factor = 1;
-	for (int i = 0; i < unHashed.getName().length(); i++)
-	{
+	for (int i = 0; i < unHashed.getName().length(); i++){
 		if (i % 4 == 0){
 			factor = 1;
 		}
@@ -64,19 +71,25 @@ int Hashmap::hashPlayer(player unHashed, int buckets) {
 }
 
 player Hashmap::retrievePlayerInfo(string name) {
-
+	for (int i = 0; i < this->hashMapBuckets.size(); i++){
+		for (int z = 0; z < this->hashMapBuckets.at(i).size(); z++){
+			string check = hashMapBuckets.at(i).at(z).getName();
+			if (check == name)
+			{
+				return hashMapBuckets.at(i).at(z);
+			}
+		}
+	}
 }
 
 vector<vector<player>> Hashmap::resize() {
 	vector<player> z;
 	this->buckets *= 2;
 	vector<vector<player>> newHashMap;
-	for (int i = 0; i < this->buckets; i++)
-	{
+	for (int i = 0; i < this->buckets; i++){
 		newHashMap.push_back(z);
 	}
-	for (int i = 0; i < this->hashMapBuckets.size(); i++)
-	{
+	for (int i = 0; i < this->hashMapBuckets.size(); i++){
 		for (int j = 0; j < this->hashMapBuckets.at(i).size(); j++)
 			insertPlayer(this->hashMapBuckets.at(i).at(j), newHashMap);
 	}
