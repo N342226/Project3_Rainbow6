5fdd99d68af5cd2f52539998fbde6e6a15a0ad2f
Robert Obendorf <robertobendorf210@gmail.com>
2020-12-01 15:12:22 -0500

    implimented random db

--- Project3_Rainbow6/Project3_Rainbow6/player.h
+++ Project3_Rainbow6/Project3_Rainbow6/player.h
@@ -7,23 +7,31 @@ using namespace std;
 class player {
 private:
 	unordered_map<string, float> operatorKd;
-	float minKd, maxKd;
 	string name;
 public:
-	player();
+	player(string name, float minKd = .5, float maxKd = 2);
+	player(string name, unordered_map<string, float> operatorKd);
 	string getName();
+	float getOperatorKd(string operatorName);
 };
 
-player::player() {
-	minKd = .5;
-	maxKd = 2;
-
+player::player(string name, float minKd, float maxKd) {
+	this->name = name;
 	for (int i = 0; i < sizeof(operators); i++) { //operators comes from operators.h
 		float kd = minKd + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (maxKd - minKd)));
 		operatorKd[operators[i]] = kd;
 	}
 }
 
+player::player(string name, unordered_map<string, float> operatorKd) {
+	this->name = name;
+	this->operatorKd = operatorKd;
+}
+
 string player::getName() {
 	return this->name;
+}
+
+float player::getOperatorKd(string operatorName) {
+	return operatorKd[operatorName];
 }
